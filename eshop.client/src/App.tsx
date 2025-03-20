4df2588a91ee55c264abe6522b958dc8de7aaa08
Mike Hatskevich <mike.hatskevich@gmail.com>
2025-03-20 17:29:42 +0200

    Add react-router-dom for routing, implement app structure with public and protected routes

--- eshop.client/src/App.tsx
+++ eshop.client/src/App.tsx
@@ -2,12 +2,36 @@ import "./App.css";
 import "./styles/index.scss";
 import Login from "./components/login";
 import { Header } from "./components/header";
+import { Navigate, Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { ProductsPage } from "./components/productsPage";
+import { ProductDetailsPage } from "./components/productDetailsPage";
+import { useAuth } from "./hooks/useAuth";
+import { UserDetailsPage } from "./components/userDetailsPage";
+import { CheckoutPage } from "./components/checkoutPage";
+import { ShoppingCartPage } from "./components/shoppingCartPage";
 
 function App() {
+  const { isAuthenticated, loading } = useAuth();
+
+  if (loading) return <div>Loading...</div>; // Prevents flickering
   return (
     <>
       <Header />
-      <Login />
+      <Router>
+        <Routes>
+          {/* Public Routes */}
+          <Route path="/" element={<Login />} />
+          <Route path="/products" element={<ProductsPage />} />
+          <Route path="/products/:id" element={<ProductDetailsPage />} />
+          {/* Protected Routes */}
+          <Route path="/user" element={isAuthenticated ? <UserDetailsPage /> : <Navigate to="/login" />} />
+          <Route path="/checkout" element={isAuthenticated ? <CheckoutPage /> : <Navigate to="/login" />} />
+          <Route path="/cart" element={<ShoppingCartPage />} />
+
+          {/* Default Redirect */}
+          <Route path="*" element={<Navigate to="/products" />} />
+        </Routes>
+      </Router>
     </>
   );
 }
