cd2086328f242255922390afbfa5d62e28938edc
Mike Hatskevich <mike.hatskevich@gmail.com>
2025-04-12 22:20:38 +0300

    Refactor code structure for improved readability and maintainability

--- eshop.admin/src/components/login.tsx
+++ eshop.admin/src/components/login.tsx
@@ -1,5 +1,4 @@
 import { useForm } from "react-hook-form";
-// import { getCart, postLogin } from "../utils/service";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 
--- eshop.admin/vite.config.ts
+++ eshop.admin/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig(({ command, mode }) => {
+export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
   return {
     define: {
