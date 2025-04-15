72ef5fa9c931293c14ccfc0009aefdcaf9413d2c
Mike Hatskevich <mike.hatskevich@gmail.com>
2025-04-15 11:02:05 +0300

    feat: implement order fetching and display in DataGrid component

--- eshop.admin/src/components/orders.tsx
+++ eshop.admin/src/components/orders.tsx
@@ -1,8 +1,43 @@
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import { useEffect, useState } from "react";
+import { httpGet } from "../utils/service";
+
+interface OrderItem {
+  userId: string;
+  shipping: number;
+  subtotal: number;
+  total: number;
+  status: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
 export function Orders() {
+  const [orders, setOrders] = useState<OrderItem[]>([]);
+  useEffect(() => {
+    const fetchOrders = async () => {
+      const res = await httpGet("order");
+      if (res.status === 404) {
+        setOrders([]);
+        return;
+      }
+
+      setOrders(res);
+    };
+
+    fetchOrders();
+  }, []);
+  const columns: GridColDef[] = [
+    { field: "shipping", headerName: "Shipping" },
+    { field: "subtotal", headerName: "Sub Total" },
+    { field: "total", headerName: "Total" },
+    { field: "status", headerName: "Status" },
+    { field: "createdAt", headerName: "Created at" },
+    { field: "updatedAt", headerName: "Updated at" },
+  ];
   return (
     <div>
       <h1>Orders</h1>
-      <p>List of orders will be displayed here.</p>
+      <DataGrid rows={orders} columns={columns} />
     </div>
   );
 }
