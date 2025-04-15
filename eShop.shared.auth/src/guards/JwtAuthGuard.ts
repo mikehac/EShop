f64f7dffc6645584f238ad67c5674afae1799b63
Mike Hatskevich <mike.hatskevich@gmail.com>
2025-04-15 12:26:12 +0300

    feat: improve error logging in JwtAuthGuard for better debugging

--- eShop.shared.auth/src/guards/JwtAuthGuard.ts
+++ eShop.shared.auth/src/guards/JwtAuthGuard.ts
@@ -5,6 +5,9 @@ import { AuthGuard } from "@nestjs/passport";
 export class JwtAuthGuard extends AuthGuard("jwt") {
   handleRequest(err: Error | null, user: any, info: any): any {
     if (err || !user) {
+      console.error(`JWT Auth Guard Error: ${err}`);
+      console.error(`JWT Auth Guard User: ${user}`);
+      console.error(`JWT Auth Guard Info: ${info}`);
       throw new UnauthorizedException("Access Denied! Invalid Token.");
     }
     return user;
