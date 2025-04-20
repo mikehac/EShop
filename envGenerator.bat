a61ad65cdc893255768d48c002fcf92fa97336ea
Mike Hatskevich <mike.hatskevich@gmail.com>
2025-04-20 14:27:44 +0300

    feat: enhance envGenerator script to create .env and .env.order files

--- envGenerator.bat
+++ envGenerator.bat
@@ -4,6 +4,28 @@ setlocal enabledelayedexpansion
 :: Save the starting directory
 set "startDir=%cd%"
 
+:: Create the eshop.db folder
+if not exist "eshop.db" (
+    mkdir "eshop.db"
+    echo Created folder: eshop.db
+)
+
+:: Create .env file in eshop.db
+(
+    echo POSTGRES_USER=postgress
+    echo POSTGRES_PASSWORD=VERY_SECURED_PASSWORD
+    echo POSTGRES_DB=eshop
+) > "eshop.db\.env"
+echo Created file: eshop.db\.env
+
+:: Create .env.order file in eshop.db
+(
+    echo POSTGRES_USER=postgress
+    echo POSTGRES_PASSWORD=VERY_SECURED_PASSWORD
+    echo POSTGRES_DB=eshop
+) > "eshop.db\.env.order"
+echo Created file: eshop.db\.env.order
+
 :: Loop through all subfolders and find .env.example files
 for /r %%f in (*.env.example) do (
     echo Found: %%f
