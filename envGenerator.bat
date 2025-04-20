d9b99348c5312f579501049d683d80b48b5c1adc
Mike Hatskevich <mike.hatskevich@gmail.com>
2025-04-20 15:06:34 +0300

    refactor: streamline envGenerator.bat and remove obsolete .env.example files for eshop.client and eshop.admin

--- envGenerator.bat
+++ envGenerator.bat
@@ -4,13 +4,6 @@ setlocal enabledelayedexpansion
 :: Save the starting directory
 set "startDir=%cd%"
 
-:: Create the eshop.db folder
-if not exist "eshop.db" (@echo off
-setlocal enabledelayedexpansion
-
-:: Save the starting directory
-set "startDir=%cd%"
-
 :: Loop through all subfolders and find .env.example files
 for /r %%f in (*.env.example) do (
     echo Found: %%f
@@ -22,67 +15,23 @@ for /r %%f in (*.env.example) do (
     if !errorlevel! equ 0 (
         echo Renaming .env.example to .env in eshop.client
         ren "%%f" ".env"
-        goto :continue
-    )
-
-    echo !folderName! | findstr /i "\\eshop.admin\\" >nul
-    if !errorlevel! equ 0 (
-        echo Renaming .env.example to .env in eshop.admin
-        ren "%%f" ".env"
-        goto :continue
-    )
-
-    :: For all other folders, copy .env.example to .env.local
-    set "targetFile=%%~dpf.env.local"
-    echo Copying !sourceFile! to !targetFile!
-    copy "!sourceFile!" "!targetFile!" >nul
-
-    if !errorlevel! equ 0 (
-        echo Successfully copied to !targetFile!
     ) else (
-        echo Failed to copy !sourceFile!
-    )
+        echo !folderName! | findstr /i "\\eshop.admin\\" >nul
+        if !errorlevel! equ 0 (
+            echo Renaming .env.example to .env in eshop.admin
+            ren "%%f" ".env"
+        ) else (
+            :: For all other folders, copy .env.example to .env.local
+            set "targetFile=%%~dpf.env.local"
+            echo Copying !sourceFile! to !targetFile!
+            copy "!sourceFile!" "!targetFile!" >nul
 
-    :continue
-)
-
-:: Return to the starting directory
-cd /d "%startDir%"
-echo Done.
-pause
-    mkdir "eshop.db"
-    echo Created folder: eshop.db
-)
-
-:: Create .env file in eshop.db
-(
-    echo POSTGRES_USER=postgress
-    echo POSTGRES_PASSWORD=VERY_SECURED_PASSWORD
-    echo POSTGRES_DB=eshop
-) > "eshop.db\.env"
-echo Created file: eshop.db\.env
-
-:: Create .env.order file in eshop.db
-(
-    echo POSTGRES_USER=postgress
-    echo POSTGRES_PASSWORD=VERY_SECURED_PASSWORD
-    echo POSTGRES_DB=eshop
-) > "eshop.db\.env.order"
-echo Created file: eshop.db\.env.order
-
-:: Loop through all subfolders and find .env.example files
-for /r %%f in (*.env.example) do (
-    echo Found: %%f
-    set "sourceFile=%%f"
-    set "targetFile=%%~dpf.env.local"
-
-    echo Copying !sourceFile! to !targetFile!
-    copy "!sourceFile!" "!targetFile!" >nul
-
-    if %errorlevel% equ 0 (
-        echo Successfully copied to !targetFile!
-    ) else (
-        echo Failed to copy !sourceFile!
+            if !errorlevel! equ 0 (
+                echo Successfully copied to !targetFile!
+            ) else (
+                echo Failed to copy !sourceFile!
+            )
+        )
     )
 )
 
