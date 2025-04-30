bd0d54f15371b96458bab80edb77eae4b31d6e2c
Mike Hatskevich <mike.hatskevich@gmail.com>
2025-04-30 16:00:47 +0300

    refactor: remove LocalizationProvider from main component for simplified rendering

--- eshop.admin/src/main.tsx
+++ eshop.admin/src/main.tsx
@@ -2,13 +2,9 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
-import { LocalizationProvider } from "@mui/x-date-pickers";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <App />
-    </LocalizationProvider>
+    <App />
   </BrowserRouter>
 );
